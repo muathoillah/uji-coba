db77954ccc1aa850a2c07bb5dbb6c285bc462701
muathoillah <faizathoilah759@gmail.com>
2023-09-22 11:24:10 +0700

    fais

--- minggu4/Circle17.java
+++ minggu4/Circle17.java
@@ -3,14 +3,14 @@ import java.util.Scanner;
 
 public class Circle17 {
     public static void main(String[] args) {
-        Scanner sc= new Scanner(System.in);
+        Scanner input= new Scanner(System.in);
         int r;
         double circumference, area;
 
         System.out.println("input radius :");
-        r = sc.nextInt();
+        r = input.nextInt();
         circumference = 2*3.14*r;
-        area = 2*r*r;
+        area = 3.14*r*r;
         System.out.println("this area :" + area);
         System.out.println("this circumference :" +circumference);
     }
